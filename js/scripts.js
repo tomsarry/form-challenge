5462e4b2e8e0ac295b5a25314389fe19c90cb707
tomsarry <55455075+tomsarry@users.noreply.github.com>
2020-06-14 14:23:45 +0200

    added a real dropdown

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,10 @@ var checkbox = document.querySelector("#data-structures");
 
 checkbox.addEventListener("change", function() {
     if (this.checked) {
-        dropdown.style.display = "flex";
-        // dropdown.style.opacity = "1";
-        dropdown.style.height = "auto";
+        data.style.display = "flex";
+        data.style.height = "auto";
     } else {
-        dropdown.style.display = "none";
-        // dropdown.style.opacity = "0";    
-        dropdown.style.height = "0px"; 
+        data.style.display = "none";  
+        data.style.height = "0px"; 
     }
 });
